0d35bb7c39abb384b593f24243fcbc4c3f0643bb
L250833@lhr.nu.edu.pk <masgharimamsyed@gmail.com>
2026-05-10 16:35:30 +0500

    Students,teacher,courses vectors and relationship bw them

--- 2nd semester project/source.cpp
+++ 2nd semester project/source.cpp
@@ -3,11 +3,6 @@
 #include<fstream>
 #include<cstring>
 #include<string>
-vector <teacher*> TEACHERS;
-vector <student*> STUDENTS;
-vector <course*> COURSES;
-vector<venue*>VENUES;
-vector <Assessment*> ASSESS;
 
 
 using namespace std;
@@ -26,7 +21,7 @@ class teacher :public academicEntity {
 		float avgScore;
 	float score;
 public:
-	teacher(int ID=0, string name,string email,float avgScore=0) {
+	teacher(int ID=0, string name="", string email = "", float avgScore = 0) {
 		this->score = 0;
 		this->ID = ID;
 		this->name = name;
@@ -44,6 +39,9 @@ public:
 		}
 		avgScore = sum / arrayofScore.size();
 	}
+	string getname() {
+		return name;
+	}
 };
 
 class student :public academicEntity {
@@ -52,12 +50,14 @@ protected:
 	string type;
 public:
 	virtual void display() = 0;
-	virtual void setGPA() = 0;
+	virtual void setGPA(int GPA) = 0;
+	friend void viewStudents();
+
 };
 class scholarshipStudent :public student {
 	bool probation;
 public:
-	scholarshipStudent(int ID=0, const char name[]="", const char email[]="",float GPA=0) {
+	scholarshipStudent(int ID=0, string name="", string email="",float GPA=0){
 		this->ID = ID;
 		this->name = name;
 		this->email = email;
@@ -70,7 +70,7 @@ public:
 			probation = false;
 		}
 	}
-
+	friend void viewStudents();
 	void display() {
 		cout <<"Id:"<<ID << ",name:" << name << ",email:" << email << ",GPA:" << GPA <<",type:"<<type << ",Probation:" << probation << endl;
 	}
@@ -89,11 +89,12 @@ public:
 class exchangeStudent :public student {
 	bool fail;
 public:
-	exchangeStudent(int ID=0, const char name[]="", const char email[] = "", float GPA = 0) {
+	exchangeStudent(int ID=0, string  name="", string email = "", float GPA = 0) {
 		this->ID = ID;
 		this->name = name;
 		this->email = email;
 		this->type = "exchange";
+		this->GPA = GPA;
 		if (GPA < 2) {
 			fail=true;
 		}
@@ -101,7 +102,7 @@ public:
 			fail = false;
 		}
 	}
-	
+	friend void viewStudents();
 	void display() {
 		cout << "Id:" << ID << ",name:" << name << ",email:" << email << ",GPA:" << GPA << ",type:" << type << ",fail:" << fail << endl;
 	}
@@ -117,26 +118,82 @@ public:
 	}
 };
 class regularStudent :public student {
-	bool feepay;
+	bool feeStatus;
 public:
-	regularStudent(int ID = 0, const char name[] = "", const char email[] = "", float GPA = 0) {
+	regularStudent(int ID = 0, string name = "", string email= "", float GPA = 0) {
 		this->ID = ID;
 		this->name = name;
 		this->email = email;
 		this->type = "regular";
-		feepay = true;
+		this->GPA = GPA;
+		feeStatus = true;
 	}
 
 	void display() {
-		cout << "Id:" << ID << ",name:" << name << ",email:" << email << ",GPA:" << GPA << ",type:" << type << ",feepay:" << feepay << endl;
+		cout << "Id:" << ID << ",name:" << name << ",email:" << email << ",GPA:" << GPA << ",type:" << type << ",feepay:" << feeStatus << endl;
 	}
-
+	friend void viewStudents();
 	void setGPA(int GPA) {
 		this->GPA = GPA;
 	}
 
-	void feepay(bool feepay) {
-		this->feepay = feepay;
+	void setfeeStatus(bool fee) {
+		feeStatus= fee;
+	}
+};
+
+class course :public teacher {
+protected:
+	int code;
+	string name, type;
+	teacher* obj;
+public:
+	course() {}
+	virtual void display() = 0;
+
+};
+class core :public course {
+
+public:
+	core(int code, string name, teacher* obj) {
+		this->code = code;
+		this->name = name;
+		this->obj = obj;
+		this->type = "core";
+	}
+
+	void display() {
+		cout << "code:" << code << ",name:" << name << ",type:" << type << ",teacher:" << obj->name;
+	}
+
+};
+class elective :public course {
+
+public:
+	elective(int code, string name, teacher* obj) {
+		this->code = code;
+		this->name = name;
+		this->obj = obj;
+		this->type = "elective";
+	}
+
+	void display() {
+		cout << "code:" << code << ",name:" << name << ",type:" << type << ",teacher:" << obj->name;
+	}
+
+};
+class lab :public course {
+
+public:
+	lab(int code, string name, teacher* obj) {
+		this->code = code;
+		this->name = name;
+		this->obj = obj;
+		this->type = "lab";
+	}
+
+	void display() {
+		cout << "code:" << code << ",name:" << name << ",type:" << type << ",teacher:" << obj->name;
 	}
 };
 
@@ -160,60 +217,7 @@ class quizzes :public Assessment {};
 class assignments :public Assessment {
 };
 
-class course:public teacher{
-protected:
-	int code;
-	string name, type;
-	teacher* obj;
-public:
-	course() {}
-	virtual void display() = 0;
 
-};
-class core :public course {
-
-public:
-	core(int code,string name,teacher *obj) {
-		this->code = code;
-		this->name = name;
-		this->obj = obj;
-		this->type = "core";
-	}
-
-	void display() {
-		cout << "code:" << code << ",name:" << name << ",type:" << type << ",teacher:" << obj->name;
-	}
-
-};
-class elective :public course {
-	
-public:
-	elective(int code, string name, teacher* obj) {
-		this->code = code;
-		this->name = name;
-		this->obj = obj;
-		this->type = "elective";
-	}
-
-	void display() {
-		cout << "code:" << code << ",name:" << name << ",type:" << type << ",teacher:" << obj->name;
-	}
-	
-};
-class lab :public course {
-	
-public:
-	lab(int code, string name, teacher* obj) {
-		this->code = code;
-		this->name = name;
-		this->obj = obj;
-		this->type = "lab";
-	}
-
-	void display() {
-		cout << "code:" << code << ",name:" << name << ",type:" << type << ",teacher:" << obj->name;
-	}
-};
 
 class venue {
 protected:
@@ -221,7 +225,7 @@ protected:
 	int capacity;
 	bool computer;
 public:
-	venue(int roomno = 0, int capacity = 0, bool computer) {
+	venue(int roomno = 0, int capacity = 0, bool computer=0) {
 		this->roomno = roomno;
 		this->capacity = capacity;
 		this->computer = computer;
@@ -232,48 +236,172 @@ public:
 };
 
 
-#include <iostream>
-using namespace std;
 
-void addStudent();
-void viewStudents();
-void addTeacher();
-void viewTeachers();
-void addCourse();
-void viewCourses();
+vector <teacher*> TEACHERS;
+vector <student*> STUDENTS;
+vector <course*> COURSES;
+vector <venue*>VENUES;
+vector <Assessment*> ASSESS;
+
+void loadData();
+void addStudent();//
+void viewStudents();//
+void addTeacher();//
+void viewTeachers();//
+void addCourse();//
+void viewCourses();//
 void registerStudentInCourse();
 void createSection();
 void assignCourseToTeacher();
 void saveData();
 
+void loadData() {
+
+}
+
 void addStudent()
 {
+	int ID;
+	string name;
+	string email;
+	float GPA;
+	int type;
+
+	cout << "Enter ID:" << endl;
+	cin >> ID;
+	cout << "Enter Name:" << endl;
+	cin >> name;
+	cout << "Enter email:" << endl;
+	cin >> email;
+	cout << "Enter GPA:" << endl;
+	cin >> GPA;
+
+	cout << "Enter Type:" << endl;
+	cout << "1. Regular Student\n";
+	cout << "2. Scholarship Student\n";
+	cout << "3. Exchange Student\n";
+
+	cin >> type;
+	while (type != 1 && type != 2 && type != 3) {
+		cout << "Inavlid input ,input again:" << endl;
+		cin >> type;
+	}
+	
+	if (type == 1)
+	{
+		
+		STUDENTS.push_back(new regularStudent(ID,name,email,GPA));
+
+		cout << "\nRegular Student Added!\n";
+	}
+
+	else if (type == 2)
+	{
+		STUDENTS.push_back(new scholarshipStudent(ID,name,email,GPA));
+
+		cout << "\nScholarship Student Added!\n";
+	}
+
+	else if (type == 3)
+	{
+		STUDENTS.push_back(new exchangeStudent(ID,name,email,GPA));
+
+		cout << "\nExchange Student Added!\n";
+	}
 
 }
 
 void viewStudents()
 {
-	
+	for (int i = 0; i < STUDENTS.size(); i++) {
+		 (*STUDENTS[i]).display();
+	}
+
+	system("pause");
 }
 
 void addTeacher()
 {
+	int ID;
+	string name;
+	string email;
 
+
+	cout << "Enter ID:" << endl;
+	cin >> ID;
+	cout << "Enter Name:" << endl;
+	cin >> name;
+	cout << "Enter email:" << endl;
+	cin >> email;
+
+
+	
+		TEACHERS.push_back(new teacher(ID, name, email));
+
+		cout << "\nExchange Student Added!\n";
 }
 
 void viewTeachers()
 {
+	for (int i = 0; i < TEACHERS.size(); i++) {
+		 (*TEACHERS[i]).display();
+	}
 
+	system("pause");
 }
 
 void addCourse()
 {
+	int code; string name;
+	cout << "Enter code of Course:" << endl;
+	cin >> code;
+	cout << "Enter name of course:" << endl;
+	cin >> name;
 
+	int type;
+	
+	int type;
+	cout << "\nEnter Course Type:\n";
+	cout << "1. Core Course\n";
+	cout << "2. Elective Course\n";
+	cout << "3. Lab Course\n";
+
+	cout << "Choice: ";
+	cin >> type;
+	while (type != 1 && type != 2 && type != 3) {
+		cout << "Inavlid input ,input again:" << endl;
+		cin >> type;
+	}
+	viewTeachers();
+	string t_name;
+	teacher* obj;
+	cout << "Select Teacher by name which teaches this course:" << endl;
+	cin>>t_name;
+
+	for (int i = 0; i < TEACHERS.size(); i++) {
+		if (((*TEACHERS[i]).getname()) == t_name){
+			obj = TEACHERS[i];
+		}
+	}
+	if (type == 1)
+	{
+		COURSES.push_back(new core(code,name,obj));
+	}
+	else if (type == 2)
+	{
+		COURSES.push_back(new elective(code,name,obj));
+	}
+	else if (type == 3)
+	{
+		COURSES.push_back(new lab(code,name,obj));
+	}
 }
 
 void viewCourses()
 {
-
+	for (int i = 0; i < TEACHERS.size(); i++) {
+		TEACHERS[i]->display();
+	}
 }
 
 void registerStudentInCourse()
@@ -293,6 +421,7 @@ void assignCourseToTeacher()
 
 void saveData()
 {
+
 
 }
 
